461f16ca038a507fc89b1683758ebbcc528c5b71
robaadekings <146726696+robaadekings@users.noreply.github.com>
2025-05-04 01:19:03 -0700

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,42 +1,51 @@
 -- QUETION1
 -- Create a new 1NF table
-CREATE TABLE OrderDetails_1NF AS
-SELECT 
-    OrderID,
-    CustomerName,
-    TRIM(Product) AS Product
-FROM (
-    SELECT 
-        OrderID,
-        CustomerName,
-        UNNEST(STRING_TO_ARRAY(Products, ',')) AS Product
-    FROM ProductDetail
-) AS split_products;
-
--- QUETION2
-
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 
 
--- Create Orders table (contains order information)
+
+-- Question 2
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY,
     CustomerName VARCHAR(100)
 );
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(102, 'Jane Smith'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark'),
 
--- Create OrderItems table (contains product information per order)
-CREATE TABLE OrderItems (
-    OrderItemID SERIAL PRIMARY KEY,
+CREATE TABLE Product (
     OrderID INT,
-    Product VARCHAR(50),
+    Product VARCHAR(100),
     Quantity INT,
+    PRIMARY KEY (OrderID, Product),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
+INSERT INTO Product (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
 
-INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
-FROM OrderDetails;
 
-INSERT INTO OrderItems (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
+
